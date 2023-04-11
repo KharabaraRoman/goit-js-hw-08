f4dd9014badba843b51e6ce831d1da05e5271bce
Роман Харабара <117296212+KharabaraRoman@users.noreply.github.com>
2023-04-11 12:55:58 +0200

    ok

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,21 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+
+player.on('timeupdate', function(data) {
+  const currentTime = data.seconds;
+  // Зберегти час відтворення в локальне сховище
+  localStorage.setItem('videoplayer-current-time', currentTime);
+});
+
+const savedTime = localStorage.getItem('videoplayer-current-time');
+if (savedTime) {
+  player.setCurrentTime(savedTime);
+}
+player.on('timeupdate', throttle(function(data) {
+  const currentTime = data.seconds;
+  // Зберегти час відтворення в локальне сховище
+  localStorage.setItem('videoplayer-current-time', currentTime);
+}, 1000));
